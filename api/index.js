47f76d4f611eba3e6c646b86c293d654a903b03b
Dev Nasfam <logic@logics-MacBook-Pro.local>
2025-01-14 18:12:21 +0100

    corrected!

--- api/index.js
+++ api/index.js
@@ -6,12 +6,12 @@ app.get('/api', (req, res) => {
     res.status(200).json({ message: 'Hello from API' });
 });
 
-app.post('/moralis/webhook', (req, res) => {
+app.post('/api/moralis', (req, res) => {
     console.log(req.body);
     res.status(200).json({ message: 'Webhook is running, ready to start implementing logic.' });
 });
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
 });
