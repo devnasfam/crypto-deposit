5e9746f5620266f63c54249b9b0bd98a0298b980
Dev Nasfam <logic@logics-MacBook-Pro.local>
2025-01-14 18:18:57 +0100

    corrected!

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
-
+import bodyParser from 'body-parser';
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/api', (req, res) => {
     res.status(200).json({ message: 'Hello from API' });
