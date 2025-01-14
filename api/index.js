01835da6508d06dfd56ddbb1056c8d9dc7c6273e
Dev Nasfam <logic@logics-MacBook-Pro.local>
2025-01-14 17:57:52 +0100

    added get request

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from 'express'
 
 const app = express();
 
+app.get('/api', (req, res) => {
+    res.status(200).json({ message: 'Hello from API' });
+});
 
 app.post('/moralis/webhook', async (req, res) => {
     console.log(req.body);
