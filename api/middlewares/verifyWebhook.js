721f80d3730b50eb027d374f3d33cb7c67a51085
Dev Nasfam <logic@logics-MacBook-Pro.local>
2025-01-15 10:49:13 +0100

    fixed

--- api/middlewares/verifyWebhook.js
+++ api/middlewares/verifyWebhook.js
@@ -7,11 +7,13 @@ const secret = process.env.MORALIS_WEBHOOK_SECRET;
 export const verifySignature = (req, res, next) => {
     const providedSignature = req.headers["x-signature"];
     if (!providedSignature) {
+        console.log("Signature not provided");
         return res.status(401).json({ error: "Signature not provided" });
     }
     const generatedSignature = Web3.utils.sha3(JSON.stringify(req.body) + secret);
     // Compare the generated signature with the provided signature
     if (generatedSignature !== providedSignature) {
+        console.log("Invalid Signature");
         return res.status(401).json({ error: "Invalid Signature" });
     }
     // Signature is valid; proceed to the next middleware or route handler
